fc19a6dbb3804ec4f67faa546f19741470ee1dce
mtaylorii <maxs2gd4u@yahoo.com>
2025-09-10 14:57:46 -0400

    Update keymap.c
    Moved WASD layer higher so that it can use the LWR and UPR layers

--- qmk_firmware/keyboards/keebio/iris_lm/keymaps/Max_v1/keymap.c
+++ qmk_firmware/keyboards/keebio/iris_lm/keymaps/Max_v1/keymap.c
@@ -50,8 +50,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
+
+  /* Used to emulate a typical QWERTY layout that's only used for gaming, but shifted over one column so I don't have to move my left hand */
+  [WASD] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_ESC,  XXXXXXX, KC_1,    KC_2,   KC_3,     KC_4,                               KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                               KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX, KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                               KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    XXXXXXX,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX, KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    XXXXXXX,          XXXXXXX, KC_B,    KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    TL_LOWR, KC_BSPC, KC_SPC,                    XXXXXXX, XXXXXXX, TL_UPPR
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
   
-  /* Fn keys, Delete, MEDIA KEYS */
+  /* Fn keys, Delete, media keys */
   [LWR] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
@@ -96,21 +111,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     TL_LOWR, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, TL_UPPR
                                 // └───/────┴────────┴────────┘                 └────────┴────────┴───/────┘
-  ),
-  
-  /* Used to emulate a typical QWERTY layout that's only used for gaming, but shifted over one column so I don't have to move my left hand */
-  [WASD] = LAYOUT(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_ESC,  XXXXXXX, KC_1,    KC_2,   KC_3,     KC_4,                               KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                               KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                               KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    XXXXXXX,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    XXXXXXX,          XXXXXXX, KC_B,    KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    TL_LOWR, KC_BSPC, KC_SPC,                    XXXXXXX, XXXXXXX, TL_UPPR
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
   
   // END OF LAYERS. ADD A COMMA TO THE ABOVE LAYER IF YOU ADD ONE BELOW IT
